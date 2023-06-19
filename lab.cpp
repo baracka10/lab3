a35b55faf71f704603675bf798615b88a92b584b
Amath2605 <93322741+Amath2605@users.noreply.github.com>
2023-06-19 13:00:13 +0000

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -11,3 +11,12 @@ int main ()
  cout<<"***        ***   ******   *******   *******    **       ** " <<endl;
  cout<<"***        ***   ******   *******   *******    *********** " <<endl;
 }
+
+// Exercice 2 du second binome
+#include <stdio.h>
+
+int main() {
+    printf("Ceci est l'exercice 2.\n");
+    // Code de l'exercice 2
+    return 0;
+}
